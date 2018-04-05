053e1600594920765432599b0747e74ea8f295ac
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-05 18:28:19 -0400

    Update install.bash

--- install.bash
+++ install.bash
@@ -3,12 +3,11 @@
 set -ev # https://docs.travis-ci.com/user/customizing-the-build/
 
 origin=$(pwd)
-package='skeleton'
 composer create-project --no-install symfony/$package $CREATE_PROJECT_DIRECTORY
 if [[ -v $CREATE_PROJECT_DIRECTORY ]]; then
   cd $CREATE_PROJECT_DIRECTORY
 else
-  cd $package
+  cd $PACKAGE
 fi
 pwd
 (cd $origin/etap/install && tar --exclude-vcs --create --file - .) | tar --extract --verbose --file -
